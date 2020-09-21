2e368324fe1b2d8f4bfd0b4935d3fc65a2ba3753
sergiooo97 <pushooooo@gmail.com>
2020-09-21 04:11:51 -0500

    animación de una maldita flecha en welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,8 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" />
+
   <title>Centro Educativo Natanael</title>
   <!-- Font Awesome -->
   <!--<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">-->
