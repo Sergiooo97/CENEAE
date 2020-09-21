fafa4ac09f0781bd9147ef35aa365ef080670e13
sergiooo97 <pushooooo@gmail.com>
2020-09-21 03:44:16 -0500

    animación de una maldita flecha en welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -337,10 +337,10 @@ src: url("../fonts/NorthernTerritories.ttf") format("truetype");
           
     <div style="display: none; margin-top:15%;"  class="aviso-inscripcion">
       
-      <p>Inscripciones para alumnos de primaria, hasta el 28 de septiembre.</p>
+      <p class="info-inscripcion">Inscripciones para alumnos de primaria, hasta el 28 de septiembre.</p>
       
     </div>
-    <p style="position: absolute; top:85%; left:45%;">Mas información</p>
+    <p style="position: absolute; top:85%; left:40%;">Mas información</p>
     <div class="arrows"></div>
   </div>
     <!-- Content -->
